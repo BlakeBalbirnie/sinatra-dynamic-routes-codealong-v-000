58e07c1dc51884d5ef09bf690985c4a54dcd3fec
Blake Balbirnie <blakebalbirnie@gmail.com>
2018-03-06 18:08:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,5 +14,10 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get '/multiply/:num1/:num2' do
+    @multiply = params[:num1].to_i * params[:num2].to_i
+    "#{@multiply
+    }"
+  end
 
 end
